155da7279187f1e9e7be794216506cbbc08966d3
Ruben <r.moronhinojosa@gmail.com>
2023-11-10 12:38:04 +0100

    fix del betis

--- src/script.js
+++ src/script.js
@@ -98,6 +98,9 @@
                 case 'Espeleño FC':
                     equipoLi.setAttribute("src","imagenes/espelenio.png");
                     break;
+                case 'Betis FC':
+                        equipoLi.setAttribute("src","imagenes/realbetis.png");
+                        break;
                 default:
                     console.log(`No se localiza la imagen del equipo ${e.target.textcontent}`);
             }
